4cf9eecad359c542a5d704eaf90fe1c4bbaf17b5
Mayankaggarwal8055 <aggarwalmayank184@gmail.com>
2025-10-26 00:21:03 +0530

    more pattern questions

---
+++ lecture-26-oct/pattern12.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    int m = n / 2;
+
+    // first part
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << " * ";
+    }
+    cout << endl;
+
+    // second part
+
+    for (int i = 1; i <= m; i++)
+    {
+        for (int j = 1; j <= m - i + 1; j++)
+        {
+            cout << " * ";
+        }
+
+        for (int j = 1; j <= 2 * i - 1; j++)
+        {
+            cout << "   ";
+        }
+
+        for (int j = 1; j <= m - i + 1; j++)
+        {
+            cout << " * ";
+        }
+        cout << endl;
+    }
+
+    // third part
+
+    for (int i = 1; i <= m - 1; i++)
+    {
+        for (int j = 1; j <= i + 1; j++)
+        {
+            cout << " * ";
+        }
+
+        for (int j = 1; j <=2*(m-i)-1; j++)
+        {
+            cout << "   ";
+        }
+        
+        for (int j = 1; j <= i + 1; j++)
+        {
+            cout << " * ";
+        }
+        cout <<endl;
+    }
+    
+
+    // fourth part
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << " * ";
+    }
+
+    return 0;
+}
---
+++ lecture-26-oct/pattern8.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        char start = 'A';
+        for (int j = n - 1; j >= i; j--)
+        {
+
+            cout << static_cast<char>((start + 1) - 1);
+            start++;
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
