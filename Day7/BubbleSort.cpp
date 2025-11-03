4d11907a0e2ab74171a9b5007aa9a4d6bd042671
Mayankaggarwal8055 <aggarwalmayank184@gmail.com>
2025-11-03 13:20:52 +0530

    bubble sort

---
+++ Day7/BubbleSort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+void bubbleSort(int arr[], int n)
+{
+    for (int i = 1; i <= n - 1; i++)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+int main()
+{
+
+    int arr[] = {50, 30, 40, 20, 10};
+    int n = sizeof(arr) / sizeof(int);
+
+    bubbleSort(arr, n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
