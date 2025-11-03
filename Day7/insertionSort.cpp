640ad552ac5a3d40200bee75753224144d040e78
Mayankaggarwal8055 <aggarwalmayank184@gmail.com>
2025-11-03 15:25:11 +0530

    insertion sort

---
+++ Day7/insertionSort.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+void selectionSort(int arr[], int n)
+{
+    for (int i = 1; i <= n - 1; i++)
+    {
+        int key = arr[i];
+        int j;
+
+        for (j = i - 1; j >= 0; j--)
+        {
+            if (arr[j] > key)
+            {
+                arr[j + 1] = arr[j];
+            }
+        }
+
+        arr[j + 1] = key;
+    }
+}
+
+int main()
+{
+
+    int arr[] = {50, 40, 30, 20, 10};
+    int n = sizeof(arr) / sizeof(int);
+
+    selectionSort(arr, n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
