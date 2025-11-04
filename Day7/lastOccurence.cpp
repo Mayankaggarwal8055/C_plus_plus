4b707bc75ac2ad9f10fec8f86ed9bc877fde5136
Mayankaggarwal8055 <aggarwalmayank184@gmail.com>
2025-11-04 13:01:37 +0530

    last occurence.cpp

---
+++ Day7/lastOccurence.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+int lastOccurence(int arr[], int n, int t)
+{
+    int s = 0;
+    int e = n - 1;
+    int ans = -1;
+
+    while (s <= e)
+    {
+
+        int m = s + (e - s) / 2;
+
+        if (arr[m] == t)
+        {
+            ans = m;
+            s = m + 1;
+        }
+        else if (t < arr[m])
+        {
+            e = m - 1;
+        }
+        else
+        {
+            s = m + 1;
+        }
+    }
+
+    return ans;
+}
+
+int main()
+{
+
+    int arr[] = {10, 20, 20, 30, 30, 30, 30, 30, 40, 50};
+    int n = sizeof(arr) / sizeof(int);
+
+    int t;
+    cin >> t;
+
+    cout << lastOccurence(arr, n, t);
+    return 0;
+}
