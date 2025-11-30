b61d14102c409a35f74f5663139a6205036b0619
Mayankaggarwal8055 <aggarwalmayank184@gmail.com>
2025-11-30 12:52:39 +0530

    c++

---
+++ TEST5/Question3.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int x;
+    cin >> x;
+
+    vector<long long> a(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    sort(a.begin(), a.end());
+
+    int i = 0;
+    int j = n - 1;
+    int gondola = 0;
+
+    while (i <= j)
+    {
+        if (a[i] + a[j] <= x)
+        {
+            i++;
+            j--;
+        }
+        else
+        {
+            j--;
+        }
+        gondola++;
+    }
+
+    cout << gondola;
+
+    return 0;
+}
